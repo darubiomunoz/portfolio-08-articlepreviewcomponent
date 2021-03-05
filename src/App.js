423b4b01d3ce92d509c7a4f266e473264a950183
Diego Rubio <darubiomunoz@gmail.com>
2021-03-05 16:25:14 -0300

    profile info section styles

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ArticlePreview from './components/ArticlePreview';
 const App = () => {
   return (
     <div className="App">
-      <ArticlePreview />
+      <ArticlePreview name="Michelle Appleton" date="28 Jun 2020" />
     </div>
   );
 }
--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -2,8 +2,10 @@ import React from 'react';
 import './styles/ArticlePreview.css';
 
 import drawersImage from '../assets/images/drawers.jpg';
+import profileImage from '../assets/images/avatar-michelle.jpg';
+import iconShare from '../assets/images/icon-share.svg';
 
-const ArticlePreview = () => {
+const ArticlePreview = ({ name, date }) => {
     return (
         <div className="card">
             <img src={drawersImage} className="card-img-top" alt="Photograph of the article representing green drawers" />
@@ -12,6 +14,14 @@ const ArticlePreview = () => {
                 touches to furniture in your home</h5>
                 <p className="card-text">Ever been in a room and felt like something was missing? Perhaps 
                 it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
+            </div>
+            <div className="card-info">
+                <img src={profileImage} className="card-img-profile" alt="Photograph of the article's writer" />
+                <div className="card-profile">
+                    <h5 className="card-name">{name}</h5>
+                    <time className="card-date">{date}</time>
+                </div>
+                <button className="card-button"><img src={iconShare} className="card-icon" alt="Share icon" /></button>
             </div>
         </div>
     );
