d67d2d4132d84a2f2e773f7ebbabb6cc5b02b0d0
Diego Rubio <darubiomunoz@gmail.com>
2021-03-05 15:15:16 -0300

    component's text section and styles

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -6,13 +6,12 @@ import drawersImage from '../assets/images/drawers.jpg';
 const ArticlePreview = () => {
     return (
         <div className="card">
-            <img src={drawersImage} className="card-img-top" alt="..." />
+            <img src={drawersImage} className="card-img-top" alt="Photograph of the article representing green drawers" />
             <div className="card-body">
                 <h5 className="card-title">Shift the overall look and feel by adding these wonderful 
                 touches to furniture in your home</h5>
                 <p className="card-text">Ever been in a room and felt like something was missing? Perhaps 
                 it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
-                <a href="/" className="btn btn-primary">Go somewhere</a>
             </div>
         </div>
     );
