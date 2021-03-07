7f7adf2fad1bbf153aff0cb2969ca41d4182b3fa
Diego Rubio <darubiomunoz@gmail.com>
2021-03-06 23:29:32 -0300

    Accessibility features and logic

--- src/components/ArticlePreview.js
+++ src/components/ArticlePreview.js
@@ -51,46 +51,56 @@ const ComponentLogic = () => {
         if(screen >= 1024 && clicked) desktopClickInactive();
     }
 
-    return { handleClick };
+    const handleBlur = () => {
+        const cardBox = document.querySelector('.card-box');
+        const cardButton = document.querySelector('.card-button');
+        const cardIcon = document.querySelector('.card-icon');
+
+        cardBox.classList.add('invisible');
+        cardButton.classList.remove('change-color-option1');
+        cardIcon.classList.remove('card-icon-active');
+    }
+
+    return { handleClick, handleBlur };
 
 }
 
 const ArticlePreview = ({ name, date }) => {
-    const { handleClick } = ComponentLogic();
+    const { handleClick, handleBlur } = ComponentLogic();
 
     return (
         <div className="card">
-            <img src={drawersImage} className="card-img-top" alt="Photograph of the article representing green drawers" />
+            <img src={drawersImage} className="card-img-top" tabindex="0" alt="Photograph of the article representing green drawers" />
             <div className="card-body">
-                <h5 className="card-title">Shift the overall look and feel by adding these wonderful 
+                <h5 className="card-title" tabindex="0">Shift the overall look and feel by adding these wonderful 
                 touches to furniture in your home</h5>
-                <p className="card-text">Ever been in a room and felt like something was missing? Perhaps 
+                <p className="card-text" tabindex="0">Ever been in a room and felt like something was missing? Perhaps 
                 it felt slightly bare and uninviting. I’ve got some simple tips to help you make any room feel complete.</p>
             </div>
-            <div className="card-info ">
-                <img src={profileImage}className="card-img-profile" alt="Photograph of the article's writer" />
+            <div className="card-info">
+                <img src={profileImage}className="card-img-profile" tabindex="0" alt="Profile photograph of the article's writer" />
                 <div className="card-profile">
-                    <h5 className="card-name">{name}</h5>
-                    <time className="card-date">{date}</time>
+                    <h5 className="card-name" tabindex="0">{name}</h5>
+                    <time className="card-date" tabindex="0">{date}</time>
                 </div>
                 <button className="card-button" onClick={event => handleClick(event)}>
-                    <img src={iconShare} className="card-icon" alt="Share icon" />
+                    <img src={iconShare} className="card-icon" tabindex="0" alt="Share icon - Press to check or close sharing options" />
                 </button>
-                <div className="card-box invisible">
-                    <p className="card-legend">S H A R E</p>
-                    <img src={iconFacebook} className="card-icon-facebook" alt="Photograph of the article's writer" />
-                    <img src={iconTwitter} className="card-icon-twitter" alt="Photograph of the article's writer" />
-                    <img src={iconPinterest} className="card-icon-pinterest" alt="Photograph of the article's writer" />
-                    <span className="triangle-down"></span>
+                <div className="card-box invisible" aria-live="polite">
+                    <p className="card-legend" tabindex="0">SHARE</p>
+                    <img src={iconFacebook} className="card-icon-facebook" tabindex="0" alt="Share in Facebook" />
+                    <img src={iconTwitter} className="card-icon-twitter" tabindex="0" alt="Share in Twitter" />
+                    <img src={iconPinterest} className="card-icon-pinterest" tabindex="0" alt="Share in Pinterest" onBlur={() => handleBlur()} />
+                    <span className="triangle-down" aria-hidden="true"></span>
             </div>
             </div>
-            <div className="card-share invisible">
-                <p className="card-legend">S H A R E</p>
-                <img src={iconFacebook} className="card-icon-facebook" alt="Photograph of the article's writer" />
-                <img src={iconTwitter} className="card-icon-twitter" alt="Photograph of the article's writer" />
-                <img src={iconPinterest} className="card-icon-pinterest" alt="Photograph of the article's writer" />
+            <div className="card-share invisible" aria-live="polite">
+                <p className="card-legend" tabindex="0">SHARE</p>
+                <img src={iconFacebook} className="card-icon-facebook" tabindex="0" alt="Share in Facebook" />
+                <img src={iconTwitter} className="card-icon-twitter" tabindex="0" alt="Share in Twitter" />
+                <img src={iconPinterest} className="card-icon-pinterest" tabindex="0" alt="Share in Pinterest" />
                 <button className="card-button-active change-color-option2" onClick={event => handleClick(event)}>
-                    <img src={iconShare} className="card-icon-active" alt="Share icon" />
+                    <img src={iconShare} className="card-icon-active" tabindex="0" alt="Share icon - Press to close sharing options" />
                 </button>
             </div>
         </div>
